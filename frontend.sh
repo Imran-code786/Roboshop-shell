172da864c6527d774255badb0fdcbf9364abc9a8
Imran-code786 <imrankhanmohammed886@gmail.com>
2025-04-22 12:51:17 -0500

    install all congigur Frontend

---
+++ frontend.sh
@@ -0,0 +1,19 @@
+echo -e "\e[33mInstalling Nginx Server\e[0m"
+yun install nginx  -y &>>/tmp/roboshop.log
+
+echo -e "\e[34m Removing old App content\e[0m"
+rm -rf /user/share/nginx/html/* &>>/tmp/roboshop.log
+
+echo -e "\35m Downloading Froentend content \e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
+
+echo -e "\35m Extract Frontend content \e[0m"
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+
+# we need to copy configure file
+
+echo -e "\35m starting Nginx server \e[0m"
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
+
