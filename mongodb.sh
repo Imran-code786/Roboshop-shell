bd1a85d20cd838d32fe5de7a928e7535dc1b8351
Imran-code786 <imrankhanmohammed886@gmail.com>
2025-04-22 13:36:00 -0500

    update mongodb listen address

--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,8 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling mongodb Server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
-#modify the config file
+echo -e "\e[33mUpdate  mongodb Listen Address\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[33mInstalling mongodb Service\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
