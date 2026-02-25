b308f525b6610887669cf9823e11304d80173307
Imran <imrankhanmohammed130@gmail.com>
2026-02-25 14:56:47 -0600

    frontend updatall contentccgb

---
+++ mongodb.sh
@@ -0,0 +1,20 @@
+
+echo -e "\e[33m Installing Nginx service\e[0m"
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+
+
+echo -e "e\[33m Installing mongodb\2[0m"
+yum install mongodb-org -y  &>>/tmp/roboshop.log
+
+echo -e "e\[33m mongodb listen address \2[0m"
+sed -i 's/127.0.0.1/0.0.0.0' /etc/mongodb.cong
+
+echo -e "\e[33mstart mongodb service\e[0m"
+systemctl enable mongod
+systemctl restart mongod
+
+
+
+
+
+
