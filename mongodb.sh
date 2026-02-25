3071d15b37acb165582d3d7bbecab33f786b84df
Imran <imrankhanmohammed130@gmail.com>
2026-02-25 15:32:28 -0600

    frontend updatall contentccgb

--- mongodb.sh
+++ mongodb.sh
@@ -5,10 +5,10 @@ echo -e "\e[33m copy mongodb file\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo   &>>/tmp/roboshop.log
 
 
-echo -e "e\[33m Installing mongodb\e[0m"
+echo -e "\e[33m Installing mongodb\e[0m"
 yum install mongodb-org -y  &>>/tmp/roboshop.log
 
-echo -e "e\[33m mongodb listen address \e[0m"
+echo -e "\e[33m mongodb listen address \e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[33mstart mongodb service\e[0m"
