4dc8464da3cbf2447c467fd7f9381fce4fa1785d
Imran <imrankhanmohammed130@gmail.com>
2026-02-25 14:41:29 -0600

    frontend updatall content

--- Frontend.sh
+++ Frontend.sh
@@ -1,19 +1,23 @@
-yum install nginx -y
+echo -e "\e[33m  Istalling Nginx ervice\e[0m"
+yum install nginx -y  &>>$/tmp/roboshop.log
 
-rm -rf /usr/share/nginx/html/*
+echo -e "\e[33m Removing old app content\e[0m"
+rm -rf /usr/share/nginx/html/*  &>>$/tmp/roboshop.log
 
+echo -e "\e[33m Downloading Frontend content\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>$/tmp/roboshop.log
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-
-
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+echo -e "\e[33m Extract Frontend conten\e[0m"
+cd /usr/share/nginx/html &>>$/tmp/roboshop.log
+unzip /tmp/frontend.zip  &>>$/tmp/roboshop.log
 
 
 
 #vim /etc/nginx/default.d/roboshop.conf
+echo -e "\e[33mUpdate Frontend config\e[0m"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf  &>>$/tmp/roboshop.log
 
-
+echo -e "\e[33mstaring nginx server\e[0m"
 systemctl enable nginx
 systemctl restart nginx
 
