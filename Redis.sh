6db6df760e00e080f8ba9ce0552946f7bebf9104
Imran <imrankhanmohammed130@gmail.com>
2026-02-25 21:12:11 -0600

    dcxcx

---
+++ Redis.sh
@@ -0,0 +1,16 @@
+
+echo -e "\e[33m Install Redis Server \e[0m"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[33m Enable Redis server \e[0m"
+yum module enable redis:6 -y
+
+echo -e "\e[33m Install Redis \e[0m"
+yum install redis
+
+echo -e "\e[33mUpdate Redis listen address \e[0m"
+sed -i 's/127.0.0.1/0.0.0.0'  /etc/redis.conf  /etc/redis/redis.conf
+
+echo -e "\e[33m Start redis server \e[0m"
+systemctl enable redis
+systemctl start redis
