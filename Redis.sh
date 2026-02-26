5ad1f4cb07211ec88774db8d09084b8c349ce784
Imran <imrankhanmohammed130@gmail.com>
2026-02-25 21:19:54 -0600

    dcxcxg

--- Redis.sh
+++ Redis.sh
@@ -9,7 +9,8 @@ echo -e "\e[33m Install Redis \e[0m"
 yum install redis
 
 echo -e "\e[33mUpdate Redis listen address \e[0m"
-sed -i 's/127.0.0.1/0.0.0.0'  /etc/redis.conf  /etc/redis/redis.conf
+#sed -i 's/127.0.0.1/0.0.0.0'  /etc/redis.conf  /etc/redis/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf
 
 echo -e "\e[33m Start redis server \e[0m"
 systemctl enable redis
