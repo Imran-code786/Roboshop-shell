f527ecbb9e5b2a7e46e9966efdabf060b558dcbd
Imran <imrankhanmohammed130@gmail.com>
2026-03-02 14:29:35 -0600

    updated

--- mysql.sh
+++ mysql.sh
@@ -2,20 +2,20 @@ source common.sh
 component=mysql
 
 
-echo -e "${color}mDisable mysql default version file${nocolor}"
+echo -e "${color}Disable mysql default version file${nocolor}"
 yum module disable ${component} -y  &>>${log_file}
 
-echo -e "${color}m setup systemd service ${nocolor}"
+echo -e "${color} setup systemd service ${nocolor}"
 cp /root/Roboshop-shell/${component}.repo /etc/yum.repos.d/${component}.repo  &>>${log_file}
 
-echo -e "${color}mInstall mysql community server${nocolor}"
+echo -e "${color}Install mysql community server${nocolor}"
 yum install ${component}-community-server -y  &>>${log_file}
 
-echo -e "${color}mstart mysql service${nocolor}"
+echo -e "${color}start mysql service${nocolor}"
 systemctl enable ${component}d  &>>${log_file}
 systemctl start ${component}d   &>>${log_file}
 
-echo -e "${color}metup mysql password${nocolor}"
+echo -e "${color}setup mysql password${nocolor}"
 ${component}_secure_installation --set-root-pass RoboShop@1   &>>${log_file}
 
 
